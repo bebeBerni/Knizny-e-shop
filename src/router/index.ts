087c138f39e3986a9114369dfe8efac1c657b127
bebeBerni <bernadett.baloghova@student.ukf.sk>
2026-01-16 15:53:32 +0100

    hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import BookList from '@/views/BookList.vue'
 import BookDetail from '@/views/BookDetail.vue'
@@ -9,7 +9,7 @@ import Contact from '@/views/Contact.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
